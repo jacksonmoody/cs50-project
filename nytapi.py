ce19dea649c574e5de6141eb4c0cf5b82c32ea2f
Jackson <jackson@jacksonmoody.com>
2022-11-27 22:38:19 -0500

    Added NYT API

---
+++ nytapi.py
@@ -0,0 +1,5 @@
+import requests
+
+response = requests.get("https://api.nytimes.com/svc/mostpopular/v2/viewed/7.json?api-key=FgKjzYiiamFAfUJMbpPnqkn7u3ManknD")
+response = response.json()
+print(response)
