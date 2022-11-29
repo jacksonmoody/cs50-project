b87d56cc9fac067139398d945ed7690f80eee75e
Jackson <jackson@jacksonmoody.com>
2022-11-29 02:18:29 -0500

    Added NYT API

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 from flask import Flask, render_template, request
 import requests
+import random
 
 app = Flask(__name__)
 
@@ -8,5 +9,6 @@ def index():
     if request.method == "POST":
         response = requests.get("https://cs50-project-backend.herokuapp.com/")
         response = response.json()
-        return render_template("results.html", response=response["message"])
+        num = random.randint(0, 19)
+        return render_template("results.html", response=response["nyt_api"]["results"][num]["url"])
     return render_template("index.html")
