c824b55c0cb7581195c33664dfff1509848a0200
Jackson <jackson@jacksonmoody.com>
2022-12-01 00:46:58 -0500

    Stylistic improvements

--- app.py
+++ app.py
@@ -9,6 +9,18 @@ def index():
     if request.method == "POST":
         response = requests.get("https://cs50-project-backend.herokuapp.com/")
         response = response.json()
-        num = random.randint(0, 19)
-        return render_template("results.html", response=response["nyt_api"][num])
+        try:
+            response = response["nyt_api"]
+            num = random.randint(0, len(response) - 1)
+            return render_template("results.html", response=response[num])
+        except:
+            return render_template("error.html")
     return render_template("index.html")
+
+@app.errorhandler(404)
+def page_not_found(e):
+    return render_template('error.html'), 404
+
+@app.errorhandler(500)
+def application_error(e):
+    return render_template('error.html'), 500
