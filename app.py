d342715e16936780125f7eaa7f11ae5b4b547bc0
Jackson <jackson@jacksonmoody.com>
2022-12-06 01:55:49 -0500

    Updated frontend

--- app.py
+++ app.py
@@ -18,14 +18,12 @@ def index():
         advanced = request.form.get("advanced")
 
         if category == None or category == "" or advanced == "False":
-            category_list = ['sports', 'art', 'technology', 'business', 'entertainment', 'science', 'politics']
+            category_list = ['sports', 'technology', 'business', 'entertainment', 'science', 'politics']
             category = random.choice(category_list)
 
         match category:
             case "sports":
                 background_image = "/static/images/sports.jpeg"
-            case "art":
-                background_image = "/static/images/art.jpg"
             case "technology":
                 background_image = "/static/images/technology.jpeg"
             case "business":
@@ -42,6 +40,7 @@ def index():
 
         response = requests.get("https://cs50-project-backend.herokuapp.com/")
         response = response.json()
+        print(response)
 
         try:
             nyt_response = response["nyt_api"][category]
@@ -51,13 +50,11 @@ def index():
             num_nyt = random.randint(0, len(nyt_response) - 1)
             num_wiki = random.randint(0, len(wiki_response) - 1)
             num_yt = random.randint(0, len(youtube_response) - 1)
-            print(response['nyt_api'])
 
             return render_template("results.html", category=category, advanced=advanced, background_image=background_image, nyt_response=nyt_response[num_nyt], wiki_response=wiki_response[num_wiki], youtube_response=youtube_response[num_yt])
        
         except Exception as e:
             print(e)
-            print(response['nyt_api'])
             return render_template("error.html")
     
     return render_template("index.html")
